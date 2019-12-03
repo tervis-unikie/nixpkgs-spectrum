23e03a94ca71a38c5341b238f774730fc3a142a7
Alyssa Ross <hi@alyssa.is>
2019-12-03 16:16:30 +0000

    start-vm.nix: test virtio-wl

--- start-vm.nix
+++ start-vm.nix
@@ -3,27 +3,33 @@ with import ./. {};
 let
   makeSquashfs = (callPackage nixos/lib/make-squashfs.nix {}).override;
 
-  kernel' = linux_latest;
+  kernel' = linux_cros;
 
   kernel = with import lib/kernel.nix { inherit lib; inherit (kernel') version; };
     kernel'.override { structuredExtraConfig = {
                          VIRTIO_PCI = yes;
                          VIRTIO_BLK = yes;
+                         VIRTIO_WL = yes;
                          SQUASHFS = yes;
                          DEVTMPFS_MOUNT = yes;
                        }; };
 
-  init = "${hello}/bin/hello";
+  init = writeShellScript "init" ''
+    set -xe
+    ${utillinux}/bin/mount -t tmpfs none /tmp
+    export XDG_RUNTIME_DIR=/tmp
+    ${sommelier}/bin/sommelier ${coreutils}/bin/env WAYLAND_DEBUG=1 ${westonLite}/bin/weston-terminal
+  '';
 
   rootfs = runCommand "rootfs" {} ''
-    mkdir dev bin
+    mkdir dev bin tmp
     ln -s ${dash}/bin/sh bin
-    (cat ${writeReferencesToFile init}; printf "%s\n" bin dev) | xargs tar -cP --hard-dereference | ${squashfs-tools-ng}/bin/tar2sqfs $out
+    (cat ${writeReferencesToFile init}; printf "%s\n" bin dev tmp) | xargs tar -cP --hard-dereference | ${squashfs-tools-ng}/bin/tar2sqfs $out
   '';
 
 in
 
 writeShellScript "crosvm" ''
   set -x
-  exec ${crosvm}/bin/crosvm run -p init=${init} --root=${rootfs} ${kernel}/bzImage
+  exec ${crosvm}/bin/crosvm run --wayland-sock=/run/user/1000/wayland-0 -p init=${init} --root=${rootfs} ${kernel}/bzImage
 ''
