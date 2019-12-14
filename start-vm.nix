68189dcb627c3a965a8b5948a49e262bf3673b2d
Alyssa Ross <hi@alyssa.is>
2019-12-14 17:38:05 +0000

    start-vm.nix: add getty service
    Having this service will make debugging and adding more services
    easier, so it's a good one to start with.  And provided a good
    motivation to make a nicer way of defining services and putting the VM
    filesystem together in Nix.

--- start-vm.nix
+++ start-vm.nix
@@ -14,15 +14,46 @@ let
                          DEVTMPFS_MOUNT = yes;
                        }; };
 
-  stage1 = writeScript "stage1" ''
+  login = writeScript "login" ''
+    #! ${execline}/bin/execlineb -s0
+    unexport !
+    ${busybox}/bin/login -p -f root $@
+  '';
+
+  services = {
+    getty.run = writeScript "getty-run" ''
+      #! ${execline}/bin/execlineb -P
+      ${busybox}/bin/getty -i -n -l ${login} 38400 ttyS0
+    '';
+
+    ".s6-svscan".finish = writeScript "init-stage3" ''
+      #! ${execline}/bin/execlineb -P
+      foreground { s6-nuke -th }
+      s6-sleep -m -- 2000
+      foreground { s6-nuke -k }
+      wait { }
+      s6-linux-init-hpr -fr
+    '';
+  };
+
+  servicesDir = with lib; runCommandNoCC "services" {} ''
+    mkdir $out
+    ${concatStrings (mapAttrsToList (name: attrs: ''
+      mkdir $out/${name}
+      ${concatStrings (mapAttrsToList (key: value: ''
+        cp ${value} $out/${name}/${key}
+      '') attrs)}
+    '') services)}
+  '';
+
+  stage1 = writeScript "init-stage1" ''
     #! ${execline}/bin/execlineb -P
     export PATH ${lib.makeBinPath
-      [ s6-linux-init s6-portable-utils s6-linux-utils s6 execline coreutils ]}
+      [ s6-linux-init s6-portable-utils s6-linux-utils s6 execline busybox ]}
     ${s6}/bin/s6-setsid -qb --
     umask 022
     if { s6-mount -t tmpfs -o mode=0755 tmpfs /run }
-    if { s6-mkdir -p /run/service/.s6-svscan }
-    if { cp ${stage3} /run/service/.s6-svscan/finish }
+    if { s6-hiercopy /etc/service /run/service }
     emptyenv -p
 
     background {
@@ -34,7 +65,7 @@ let
       export XDG_RUNTIME_DIR /run/user/0
       foreground { ${sommelier}/bin/sommelier ${hello-wayland}/bin/hello-wayland }
       importas -i ? ?
-      if { echo STATUS: $? }
+      if { s6-echo STATUS: $? }
       s6-svscanctl -6 /run/service
     }
 
@@ -43,27 +74,34 @@ let
     s6-svscan
   '';
 
-  stage3 = writeScript "stage3" ''
-    #! ${execline}/bin/execlineb -S0
-    foreground { s6-nuke -th }
-    s6-sleep -m -- 2000
-    foreground { s6-nuke -k }
-    wait { }
-    s6-linux-init-hpr -fr
+  passwd = writeText "passwd" ''
+    root:x:0:0:System administrator:/:/bin/sh
   '';
 
   rootfs = runCommand "rootfs" {} ''
-    mkdir bin dev proc run tmp
-    ln -s ${dash}/bin/sh bin
+    mkdir $out
+    cd $out
+    mkdir bin sbin dev etc proc run tmp
+    ln -s ${dash}/bin/dash bin/sh
+    ln -s ${stage1} sbin/init
+    cp ${passwd} etc/passwd
+    touch etc/login.defs
+    cp -r ${servicesDir} etc/service
+  '';
 
-    (cat ${writeReferencesToFile stage1}; printf "%s\n" bin dev proc run tmp) \
-         | xargs tar -cP --owner root:0 --group root:0 --hard-dereference \
-         | ${squashfs-tools-ng}/bin/tar2sqfs $out
+  root-squashfs = runCommand "root-squashfs" {} ''
+    cd ${rootfs}
+    (
+        grep -v ^${rootfs} ${writeReferencesToFile rootfs}
+        printf "%s\n" *
+    ) \
+        | xargs tar -cP --owner root:0 --group root:0 --hard-dereference \
+        | ${squashfs-tools-ng}/bin/tar2sqfs $out
   '';
 
 in
 
 writeShellScript "crosvm" ''
   set -x
-  exec ${crosvm}/bin/crosvm run --wayland-sock=$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY -p init=${stage1} --root=${rootfs} ${kernel}/bzImage
+  exec ${crosvm}/bin/crosvm run --wayland-sock=$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY -p init=/sbin/init --root=${root-squashfs} ${kernel}/bzImage
 ''
