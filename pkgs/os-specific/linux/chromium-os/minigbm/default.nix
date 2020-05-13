f5abbe90a3c43a430da493b93c95206b30f9a56a
Alyssa Ross <hi@alyssa.is>
2020-05-13 01:49:42 +0000

    chromiumPackages.minigbm: add missing derivation
    Looks like I forgot to commit this in
    478b6811be7a90907382eeb711d6a018fe57a03b.

---
+++ pkgs/os-specific/linux/chromium-os/minigbm/default.nix
@@ -0,0 +1,28 @@
+{ stdenv, lib, fetchFromGitiles, upstreamInfo, pkg-config, libdrm }:
+
+stdenv.mkDerivation {
+  name = "minigbm";
+  inherit (upstreamInfo) version;
+
+  src = fetchFromGitiles upstreamInfo.components."chromiumos/platform/minigbm";
+
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ libdrm ];
+
+  patchPhase = ''
+    substituteInPlace Makefile --replace /usr/include $out/include
+  '';
+
+  buildFlags = [ "ECHO=echo" "PKG_CONFIG=pkg-config" ];
+  installFlags = [ "LIBDIR=$(out)/lib" ];
+
+  enableParallelBuilding = true;
+
+  meta = with lib; {
+    description = "GBM implementation for Chromium";
+    homepage = "https://chromium.googlesource.com/chromiumos/platform/minigbm/";
+    maintainers = with maintainers; [ qyliss ];
+    license = licenses.bsd3;
+    platforms = platforms.linux;
+  };
+}
