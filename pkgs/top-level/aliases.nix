12a0b52ebb5101f9bb436a764c1ebf3d730db528
Alyssa Ross <hi@alyssa.is>
2021-04-01 17:30:29 +0000

    linux: enable Xen everywhere it can be
    I don't think there's any reason to have a seperate kernel variant
    because of this, with all the maintenance burden that imposes.  Debian
    and Fedora both enable all these options on their normal kernels.
    
    Alias the Linux Xen attributes, so this change should be seemless for
    people who were using the Xen kernels up to now.
    
    All the Xen options are marked as optional anyway, so it should be
    fine to try to enable them on non-x86 platforms as well.
    
    Cherry-picking this for Spectrum because PVH is cloud-hypervisor's
    preferred boot entry point on x86, and it'll complain if our kernel
    doesn't support it.
    
    Fixes: https://github.com/NixOS/nixpkgs/issues/115182
    
    (cherry picked from commit 5f24024de6f6405bb9ae9fc4c960b3fba84613b9)
    
    Message-Id: <20210411115740.29615-2-hi@alyssa.is>
    Reviewed-by: Cole Helbling <cole.e.helbling@outlook.com>

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -276,6 +276,12 @@ mapAliases ({
   linuxPackages_testing_hardened = throw "linuxPackages_testing_hardened has been removed, please use linuxPackages_latest_hardened";
   linux_testing_hardened = throw "linux_testing_hardened has been removed, please use linux_latest_hardened";
 
+  # added 2021-04-04
+  linuxPackages_xen_dom0 = linuxPackages;
+  linuxPackages_latest_xen_dom0 = linuxPackages_latest;
+  linuxPackages_xen_dom0_hardened = linuxPackages_hardened;
+  linuxPackages_latest_xen_dom0_hardened = linuxPackages_latest_hardened;
+
   linux-steam-integration = throw "linux-steam-integration has been removed, as the upstream project has been abandoned"; # added 2020-05-22
   loadcaffe = throw "loadcaffe has been removed, as the upstream project has been abandoned"; # added 2020-03-28
   lttngTools = lttng-tools;  # added 2014-07-31
