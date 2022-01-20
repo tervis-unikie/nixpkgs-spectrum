cdd89f7d9b60336160ff0e507fbde9438bfea315
Alyssa Ross <hi@alyssa.is>
2022-01-20 02:12:07 +0000

    qemu: put virtiofsd in bin/
    According to the QEMU documentation, virtiofsd is supposed to be run
    directly by users.  It therefore makes sense to have it in bin/, so it
    will be in PATH.  Such a change wouldn't make sense upstream, because
    it would then conflict with the virtiofsd package, which is generally
    preferred if available.  But in Nixpkgs, we don't have to worry about
    that and can just make QEMU's virtiofsd a lower priority than the one
    from the dedicated package.
    
    [1]: https://qemu.readthedocs.io/en/latest/tools/virtiofsd.html
    
    Fixes: https://github.com/NixOS/nixpkgs/issues/113172 ("QEmu: virtiofsd prone to be garbage collected.")
    Fixes: https://github.com/NixOS/nixpkgs/pull/153007 ("nixos/libvirtd: add ${cfg.qemu.package}/libexec to PATH")

--- pkgs/applications/virtualization/qemu/default.nix
+++ pkgs/applications/virtualization/qemu/default.nix
@@ -184,6 +184,7 @@ stdenv.mkDerivation rec {
 
   # Add a ‘qemu-kvm’ wrapper for compatibility/convenience.
   postInstall = ''
+    ln -s $out/libexec/virtiofsd $out/bin
     ln -s $out/bin/qemu-system-${stdenv.hostPlatform.qemuArch} $out/bin/qemu-kvm
   '';
 
@@ -201,5 +202,6 @@ stdenv.mkDerivation rec {
     mainProgram = "qemu-kvm";
     maintainers = with maintainers; [ eelco qyliss ];
     platforms = platforms.unix;
+    priority = 10; # Prefer virtiofsd from the virtiofsd package.
   };
 }
